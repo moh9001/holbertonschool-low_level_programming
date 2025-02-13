d9a01e5a1fb9fad8ed00a57fd35eb1010c8d6103
moh9001 <mo9001@gmail.com>
2025-02-13 15:34:15 +0000

    Solved task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Description prints with grammar, but the outcome is a piece of art
+ * and end it with a new line
+ * using printf
+ *
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
+
+}
