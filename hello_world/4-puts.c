c0b1e51b30b249f484281e16e13d4e24a3636311
moh9001 <mo9001@gmail.com>
2025-02-13 15:20:13 +0000

    Fixed task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void){
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+/**
+ * main - Entry point
+ *
+ * Description: Prints `"Programming is like building a multilingual puzzle"`
+ * using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
