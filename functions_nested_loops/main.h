289c43159e803c90cd346ec7a4487ebd22cdf8f5
moh9001 <mo9001@gmail.com>
2025-02-18 12:59:27 +0000

    Fix task 2 and solve task 5 and 6

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _putchar(char c);
 void print_alphabet(void); /*prints alphabets in lower case*/
 void print_alphabet_x10(void); /*prints alphabets 10x in lower case*/
 int _islower(int c); /*Checks if the charecter is lower case or not*/
-int _isalpha(int c);
+int _isalpha(int c); /*checks ig the input is a letter ot not */
+int print_sign(int n);
 
 #endif /* MAIN_H */
