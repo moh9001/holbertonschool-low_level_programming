443878777cd99100d63920d6ecbfb6c3231541b5
moh9001 <mo9001@gmail.com>
2025-02-19 13:26:09 +0000

    Fix task 12

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * print_number - Prints an integer using _putchar
+ * @num: The number to print
+ */
+void print_number(int num)
+{
+	if (num >= 100) /* If number has 3 digits */
+	{
+		_putchar((num / 100) + '0'); /* Print hundreds */
+		_putchar(((num / 10) % 10) + '0'); /* Print tens */
+	}
+	else if (num >= 10) /* If number has 2 digits */
+	{
+		_putchar(' ');
+		_putchar((num / 10) + '0'); /* Print tens */
+	}
+	else /* If number has 1 digit */
+	{
+		_putchar(' ');
+		_putchar(' ');
+	}
+	_putchar((num % 10) + '0'); /* Print ones */
+}
+
+/**
  * print_times_table - Prints the n times table, starting with 0
  * @n: The number for the times table (0 <= n <= 15)
  */
@@ -16,34 +40,13 @@ void print_times_table(int n)
 		for (col = 0; col <= n; col++)
 		{
 			product = row * col;
-
-			/* Print the first number without a leading comma */
 			if (col == 0)
-			{
-				_putchar(product + '0');
-			}
+				_putchar(product + '0'); /* Print first number */
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				/* Proper spacing */
-				if (product < 10)
-					_putchar(' ');
-				if (product < 100)
-					_putchar(' ');
-
-				/* Print product */
-				if (product >= 100)
-				{
-					_putchar((product / 100) + '0'); /* Print hundreds digit */
-					_putchar(((product / 10) % 10) + '0'); /* Print tens digit */
-				}
-				else if (product >= 10)
-				{
-					_putchar((product / 10) + '0'); /* Print tens digit */
-				}
-				_putchar((product % 10) + '0'); /* Print ones digit */
+				print_number(product); /* Use helper function */
 			}
 		}
 		_putchar('\n');
