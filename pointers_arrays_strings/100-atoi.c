847ced36737dbaca0b8af2935722d04c62098037
moh9001 <mo9001@gmail.com>
2025-02-24 10:40:12 +0000

    Fix task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,24 +10,30 @@ int	_atoi(char *s)
 {
 	int	i;
 	int	sign;
-	int	result;
+	unsigned int	result; /* Use unsigned int to handle INT_MIN */
 
 	i = 0;
 	sign = 1;
 	result = 0;
 
-	while (s[i] != '\0') /* Traverse string */
+	while (s[i] != '\0') /* Traverse the string */
 	{
-		if (s[i] == '-') /* Count minus signs */
+		if (s[i] == '-') /* Handle negative sign */
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9') /* Convert number */
 		{
 			result = (result * 10) + (s[i] - '0');
-			if (s[i + 1] < '0' || s[i + 1] > '9') /* Stop when number ends */
+			if (result > 2147483648) /* Prevent overflow */
 				break;
 		}
+		else if (result > 0) /* Stop when number ends */
+			break;
 		i++;
 	}
 
-	return (result * sign);
+	/* Handle INT_MIN case explicitly */
+	if (result == 2147483648 && sign == -1)
+		return (-2147483648);
+
+	return (sign * result);
 }
