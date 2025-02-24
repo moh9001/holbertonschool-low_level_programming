c9caea323ce7efd34d56e4b5d7e2c385426f608e
moh9001 <mo9001@gmail.com>
2025-02-24 08:55:11 +0000

    a

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -6,13 +6,20 @@
  */
 void	print_number(int n)
 {
-	if (n < 0) /* Handle negative numbers */
+	if (n == -2147483648) /* Special case handling */
+	{
+		_putchar('-');
+		_putchar('2'); /* Print '2' first */
+		n = 147483648; /* Convert to positive 147483648 */
+	}
+
+	if (n < 0) /* Handle other negative numbers */
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n / 10) /* Recursive call to remove last digit */
+	if (n / 10) /* Recursively print digits */
 		print_number(n / 10);
 
 	_putchar((n % 10) + '0'); /* Print last digit */
