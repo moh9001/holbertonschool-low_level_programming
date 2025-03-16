6cc122043324f43b83dfdc54900848aed2951a2a
moh9001 <mo9001@gmail.com>
2025-03-16 13:53:23 +0300

    Solved all tasks

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function on each element of an array
+ * @array: The array of integers
+ * @size: The number of elements in the array
+ * @action: A function pointer that takes an integer
+ *
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
---
+++ function_pointers/2-main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * is_98 - Checks if a number is equal to 98
+ * @elem: The integer to check
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int is_98(int elem)
+{
+	return (elem == 98);
+}
+
+/**
+ * is_strictly_positive - Checks if a number is greater than 0
+ * @elem: The integer to check
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int is_strictly_positive(int elem)
+{
+	return (elem > 0);
+}
+
+/**
+ * abs_is_98 - Checks if the absolute value of a number is 98
+ * @elem: The integer to check
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int abs_is_98(int elem)
+{
+	return (elem == 98 || elem == -98);
+}
+
+/**
+ * main - Tests the int_index function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+	int index;
+
+	index = int_index(array, 20, is_98);
+	printf("%d\n", index);
+
+	index = int_index(array, 20, abs_is_98);
+	printf("%d\n", index);
+
+	index = int_index(array, 20, is_strictly_positive);
+	printf("%d\n", index);
+
+	return (0);
+}
