da82d3e1e68050c39e067dff788659d378d10f13
moh9001 <mo9001@gmail.com>
2025-04-05 19:01:24 +0300

    Solved last task

---
+++ file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+
+void close_fd(int fd);
+
+/**
+ * main - copies content from file_from to file_to
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to, r_count, w_count;
+	char buffer[BUFFER_SIZE];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close_fd(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	while ((r_count = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w_count = write(fd_to, buffer, r_count);
+		if (w_count != r_count)
+		{
+			close_fd(fd_from);
+			close_fd(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+	if (r_count == -1)
+	{
+		close_fd(fd_from);
+		close_fd(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+	return (0);
+}
+
+/**
+ * close_fd - closes a file descriptor with error handling
+ * @fd: file descriptor
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
